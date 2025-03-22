26b7c67bf162b7860eeee5c4387dfa597547cbbb
Daniel McSheehy <danielmcsheehy@protonmail.com>
2025-03-21 20:53:54 -0400

    change theme of chart

--- packages/ava-react/src/InsightCard/styled/container.tsx
+++ packages/ava-react/src/InsightCard/styled/container.tsx
@@ -6,7 +6,7 @@ export const Container = styled.div`
   font-size: 12px;
   font-family: roboto-regular, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'PingFang SC', 'Hiragino Sans GB',
     'Microsoft YaHei', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  background-color: #fff;
+  background-color: rgb(45, 56, 85);
   border-radius: 2px;
-  border: 1px solid #e8e8e8;
+  border: 1px solid rgb(30, 41, 59);
 `;
